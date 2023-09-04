96b7ca7c818f791ab6b1429808065ce2f5735f88
Pavel Pereverziev <pereverzev.pavel1975@gmail.com>
2023-09-04 17:44:26 +0300

    added navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,3 +1,16 @@
-const Navigation = ()=>{}
+import { NavLink } from 'react-router-dom';
+import { useAuth } from 'hooks/useAuth';
+import { Nav } from './Navigation.styled';
+
+const Navigation = () => {
+  const { isLoggedIn } = useAuth();
+
+  return (
+    <Nav>
+      <NavLink to="/">Home</NavLink>
+      {isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
+    </Nav>
+  );
+};
 
 export default Navigation;
