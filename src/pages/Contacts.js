3490fdc6994c1030ae29105063b9b3e32fe52412
Pavel Pereverziev <pereverzev.pavel1975@gmail.com>
2023-09-05 21:45:22 +0300

    fix bug

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -2,17 +2,16 @@ import { useEffect } from 'react';
 import ContactsList from 'components/ContactsList/ContactsList';
 import Filter from 'components/Filter/Filter';
 import InputForm from 'components/InputForm/InputForm';
-import { useAuth } from 'hooks/useAuth';
 import { fetchContacts } from 'redux/contacts/operation';
 import { useDispatch } from 'react-redux';
 
 const Contacts = () => {
   const dispatch = useDispatch();
-  const { isLoggedIn } = useAuth();
+  
 
   useEffect(() => {
-    isLoggedIn && dispatch(fetchContacts());
-  }, [dispatch, isLoggedIn]);
+     dispatch(fetchContacts());
+  }, [dispatch]);
 
 
   return (
